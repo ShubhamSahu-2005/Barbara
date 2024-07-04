888a5b8746c9ca4905c375de4401fde6d6fccc45
SHUBHAM SAHU <shubhamsahu5122333@gmail.com>
2024-07-04 20:50:19 +0530

    Second commit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,10 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  let time=now.toLocaleString([],{
+    hour:'2-digit',
+    minute:'2-digit'
+  });
   const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
 
   return (
